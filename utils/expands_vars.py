1d6ce519fa7d0440c71c4d06af21e096e098b07b
sokiGit <soki.personal@gmail.com>
2025-12-05 22:05:27 +0100

    Fix Windows not escaping backslash character in paths.

--- utils/expands_vars.py
+++ utils/expands_vars.py
@@ -1,4 +1,5 @@
 import os
+import shlex
 
 def expand_vars_and_tilde(command_string):
     if not command_string:
@@ -11,6 +12,12 @@ def expand_vars_and_tilde(command_string):
     in_double = False
 
     def expand_var_name(name):
+        if os.name == 'nt':
+            for key in os.environ:
+                if key.lower() == name.lower():
+                    return os.environ[key]
+            return ""
+    
         return os.environ.get(name, "")
 
     while i < length:
@@ -41,7 +48,10 @@ def expand_vars_and_tilde(command_string):
             prev_char = command_string[i - 1] if i > 0 else None
             if prev_char is None or prev_char.isspace():
                 if i + 1 == length or command_string[i + 1] == "/" or command_string[i + 1].isspace():
-                    result.append(os.path.expanduser("~"))
+                    normalized_home = os.path.normpath(os.path.expanduser("~"))
+                    if os.name == 'nt':
+                        normalized_home = normalized_home.replace(os.sep, os.sep * 2)
+                    result.append(normalized_home)
                     i += 1
                     continue
             result.append("~")
